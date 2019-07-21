9981a83d5768f2cc358dc5d1042b3cb78c9627c0
amoyo <amoyon@hotmail.com>
2019-07-21 12:39:39 -0500

    Evento se elimina del ListView en caso de seleccionar otra fecha distinta de 23/7/2019, duración del Toast reducido a Short y corrección en la impresión de fecha (número de mes) por Toast

--- app/src/main/java/com/example/amst3/calendarActivity.java
+++ app/src/main/java/com/example/amst3/calendarActivity.java
@@ -13,8 +13,8 @@ import java.util.ArrayList;
 public class calendarActivity extends AppCompatActivity {
     private CalendarView calendarView;
     private ListView listView;
-    private EventAdapter adapter,adapterNull;
-    private ArrayList<Event> listEvents = getItems();
+    private EventAdapter adapter;
+    private ArrayList<Event> listEvents = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,28 +23,27 @@ public class calendarActivity extends AppCompatActivity {
         calendarView = (CalendarView) findViewById(R.id.calendarView);
         listView = (ListView) findViewById(R.id.listView);
         adapter = new EventAdapter(listEvents,this);
+        listView.setAdapter(adapter);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 // display the selected date by using a toast
                 if((year==2019)&&((month+1)==7)&&(dayOfMonth==23)){
-                    listEvents = getItems();
-                    listView.setAdapter(adapter);
-                    //adapter.notifyDataSetChanged();
+                    listEvents.clear();
+                    updateItems();
+                    adapter.notifyDataSetChanged();
                 }
                 else{
                     listEvents.clear();
                     adapter.notifyDataSetChanged();
                 }
-                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + (month+1) + "/" + year, Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
-        private ArrayList<Event> getItems(){
-            ArrayList<Event> listItems = new ArrayList<>();
-            listItems.add(new Event("22:00", "1h00", "Entrega de deber de PST", "ESPOL"));
-            return listItems;
+        private void updateItems(){
+            listEvents.add(new Event("22:00", "1h00", "Entrega de deber de PST", "ESPOL"));
         }
 }
