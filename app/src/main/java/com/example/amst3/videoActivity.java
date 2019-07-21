121cce502aa166956a8c3aae6de0d2707ebf0de7
amoyo <amoyon@hotmail.com>
2019-07-21 00:44:41 -0500

    Video añadido a videoActivity

--- app/src/main/java/com/example/amst3/videoActivity.java
+++ app/src/main/java/com/example/amst3/videoActivity.java
@@ -1,14 +1,28 @@
 package com.example.amst3;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.VideoView;
+
+import java.util.Vector;
 
 public class videoActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private Vector<YoutubeVideo> youtubeVideos = new Vector<YoutubeVideo>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager( new LinearLayoutManager(this));
+        youtubeVideos.add( new YoutubeVideo("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/ag8i7Cw-ehE\" frameborder=\"0\" allowfullscreen></iframe>") );
+        VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos);
+        recyclerView.setAdapter(videoAdapter);
     }
     
 }
