20ff8dfcf683115640256fc5aa88fc59a6e0c4d0
amoyo <amoyon@hotmail.com>
2019-07-21 01:20:35 -0500

    Calendario añadido a calendarActivity

--- app/src/main/java/com/example/amst3/calendarActivity.java
+++ app/src/main/java/com/example/amst3/calendarActivity.java
@@ -2,13 +2,23 @@ package com.example.amst3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class calendarActivity extends AppCompatActivity {
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-    }
-
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // display the selected date by using a toast
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+    }   
 }
