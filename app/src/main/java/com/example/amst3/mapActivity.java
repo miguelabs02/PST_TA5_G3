eeb2161bd6a99db4966a34701dccfa21b3febc99
amoyo <amoyon@hotmail.com>
2019-07-21 13:43:09 -0500

    Mapa añadido a mapActivity.

--- app/src/main/java/com/example/amst3/mapActivity.java
+++ app/src/main/java/com/example/amst3/mapActivity.java
@@ -3,12 +3,15 @@ package com.example.amst3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class mapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
     }
 
 }
