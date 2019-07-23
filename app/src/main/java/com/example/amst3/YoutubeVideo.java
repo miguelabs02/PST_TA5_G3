277905ff0ef9cfb40bcfa64e4b7de2577ba985cb
Jair <jjmarcil@espol.edu.ec>
2019-07-23 18:36:36 -0500

    Commit clase YoutubeVideo para usar en VideoAdapter

--- app/src/main/java/com/example/amst3/YoutubeVideo.java
+++ app/src/main/java/com/example/amst3/YoutubeVideo.java
@@ -1,5 +1,10 @@
 package com.example.amst3;
 
+//En crea la clase YoutubeVideo la cual tiene como atributo un String que es la
+//dirección (URL) de nuestro video de youtube
+//nos permitirá usar los metods get y set de este atributo, luego en la clase
+//VideoAdapter.
+
 public class YoutubeVideo {
     String videoUrl;
 
