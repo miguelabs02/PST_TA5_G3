c37e782fa98ace6f31c004173126e53c9abce002
Jair <jjmarcil@espol.edu.ec>
2019-07-23 19:09:15 -0500

    Commit VideoAdapter y videoActivity para la opcion de desafio del video de youtube.

--- app/src/main/java/com/example/amst3/VideoAdapter.java
+++ app/src/main/java/com/example/amst3/VideoAdapter.java
@@ -8,7 +8,10 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 import java.util.List;
-
+//Esta Clase sirve de adaptador de vista, para poder presentar el video de Youtube
+//Se añaden a los activitys como cualquier otro elemento
+//En esta clase se definen objetos view, que serán los que presenten en el activity principal
+//de esta sección.
 public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHolder> {
     List<YoutubeVideo> youtubeVideoList;
 
@@ -21,29 +24,25 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
 
     @Override
     public VideoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
         View view = LayoutInflater.from( parent.getContext()).inflate(R.layout.video_view, parent, false);
-
         return new VideoViewHolder(view);
-
     }
 
-    @Override
+    @Override//Crea un  WebView para cada elemento de la lista de YoutubeVideoList
     public void onBindViewHolder(VideoViewHolder holder, int position) {
-
         holder.videoWeb.loadData( youtubeVideoList.get(position).getVideoUrl(), "text/html" , "utf-8" );
-
     }
 
-    @Override
+    @Override//Obtiene el tamaño de la lista de videos de youtube
     public int getItemCount() {
         return youtubeVideoList.size();
     }
 
+    //Se crea el widget de WebView el cual nos permite presentar una pagina de internet, a traves de la
+    //URL, respetando y agregando los permisos del uso de internet ya antes descritos en el manifest,
+    // en nuestro caso será de la plataforma de youtube.
     public class VideoViewHolder extends RecyclerView.ViewHolder{
-
         WebView videoWeb;
-
         public VideoViewHolder(View itemView) {
             super(itemView);
 
--- app/src/main/java/com/example/amst3/videoActivity.java
+++ app/src/main/java/com/example/amst3/videoActivity.java
@@ -9,10 +9,19 @@ import android.widget.VideoView;
 
 import java.util.Vector;
 
+//Se define el videoActivity, el cual será activado luego de que en el MainActivity
+//se de click sobre el boton de Video.
+
 public class videoActivity extends AppCompatActivity {
+    //Se RecyclerView es el widget que se presenta el el diseño de este activity y muestra el video
+    //Y  el vector que tendrá los videos agregados para visualizar desde Youtube.
     private RecyclerView recyclerView;
     private Vector<YoutubeVideo> youtubeVideos = new Vector<YoutubeVideo>();
 
+    //en el metodo onCreate de este Activity se definen los atributos del widget recyclerView y
+    // se agrega un video a la lista de videos de youtube
+    //luego con la clase VideoAdapter muestra este elemento agregado y lo establece el widget
+    // principal que es el que presentará en el videoActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
